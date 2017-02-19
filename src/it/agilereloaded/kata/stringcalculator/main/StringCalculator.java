a24e31c6632de5b8f5e57b2130a702e7b2442de8
Stefano Leli <stefano.leli@agilereloaded.it>
2017-02-19 15:53:19 +0100

    refactor givenTwoCommaSeparatedNumbersShouldReturnTheirSum

--- src/it/agilereloaded/kata/stringcalculator/main/StringCalculator.java
+++ src/it/agilereloaded/kata/stringcalculator/main/StringCalculator.java
@@ -3,12 +3,21 @@ package it.agilereloaded.kata.stringcalculator.main;
 public class StringCalculator {
 
 	public int Add(String n) {
-		if(n == null || n.isEmpty()) 
-			return 0;
-		String[] numbers = n.split(",");
-		
+		String[] numbers = extractNumbersFrom(n);
+		return sumOf(numbers);
+	}
+
+	private String[] extractNumbersFrom(String n) {
+		String[] numbers = new String[0];
+		if (n != null && !n.isEmpty()) {
+			numbers = n.split(",");
+		}
+		return numbers;
+	}
+
+	private int sumOf(String[] numbers) {
 		int sum = 0;
-		for(String number : numbers)
+		for (String number : numbers)
 			sum += Integer.parseInt(number);
 		return sum;
 	}
