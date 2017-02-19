0425cd115404562af8376e2abf28428b68ec4f76
Stefano Leli <stefano.leli@agilereloaded.it>
2017-02-19 15:02:22 +0100

    Project Setup

---
+++ src/it/agilereloaded/kata/stringcalculator/test/StringCalculatorTest.java
@@ -0,0 +1,14 @@
+package it.agilereloaded.kata.stringcalculator.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringCalculatorTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
